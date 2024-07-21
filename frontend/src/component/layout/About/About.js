ae8d4b38e1f5da5b29e1847c52a7db0e7d02e0d2
vivek carpenter <105868629+Qhfdfj@users.noreply.github.com>
2024-07-21 14:04:30 +0530

    first commit

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
-import YouTubeIcon from "@material-ui/icons/YouTube";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import {FaLinkedin }  from "react-icons/fa"
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://www.instagram.com/vivek_carpenter_?utm_source=qr&igsh=MTV1bHJ3YWtyY2I0ZQ==";
   };
   return (
     <div className="aboutSection">
@@ -18,28 +18,27 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src="https://res.cloudinary.com/dlpgjh3mi/image/upload/v1721549851/vivek/dnnv1oqxrapr06czbwtp.jpg"
               alt="Founder"
             />
-            <Typography>Abhishek Singh</Typography>
+            <Typography>Vivek Carpenter</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+              This is a sample wesbite made by @vivek_carpenter_.
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              href="https://www.linkedin.com/in/vivek-carpenter-84845a242/"
               target="blank"
             >
-              <YouTubeIcon className="youtubeSvgIcon" />
+              <FaLinkedin  className="youtubeSvgIcon" />
             </a>
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
+            <a href="https://www.instagram.com/vivek_carpenter_?utm_source=qr&igsh=MTV1bHJ3YWtyY2I0ZQ==" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
             </a>
           </div>
